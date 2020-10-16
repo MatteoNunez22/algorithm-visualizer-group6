fc6c15b4c8e02cb33d4be47af8108e9c0a67f5bb
edgar c <tsung-hsun.chang@mail.mcgill.ca>
2020-10-16 16:31:43 +0800

    feat:update PopAndDequeue

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -8,5 +8,5 @@ function Homepage() {
         </div>
     );
 }
-  
+
 export default Homepage;
--- src/Components/PopAndDequeue.jsx
+++ src/Components/PopAndDequeue.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import "../Style/PopAndDequeue.css"
 
+let entry = 0;
+let stack = [];
+let queue = [];
 function PopAndDequeue() {
-    return (
-        <div>
-            <p>Pop and Dequeue</p>
-        </div>
-    );
+    // This syntax ensures `this` is bound within handleClick.
+    // Warning: this is *experimental* syntax.
+
+  return (
+    <div>
+      <p>Pop and Dequeue</p>
+      <label>
+        Entry &nbsp;
+        <input type="text" id="entry"/>
+      </label> 
+      <br/><button onClick={handleEntryChange}>
+        Initialize stack and queue
+      </button>
+    </div>
+  );
 }
-  
+
+function handleEntryChange(){
+  entry = document.getElementById("entry").value;
+  for(let i=0; i<entry; i++){
+    stack.push('a');
+    queue.push('a');
+  }
+  console.log(entry);
+  console.log(stack);
+}
+
+
 export default PopAndDequeue;
