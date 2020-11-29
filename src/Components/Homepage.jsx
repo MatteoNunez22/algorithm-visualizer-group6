c28c0c557a88e7f2dc6b0f2a140e4c7c716defd3
Matteo Nunez <matteo.nunez22@gmail.com>
2020-11-29 17:01:20 -0500

    Change homepage

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -10,7 +10,10 @@ function Homepage() {
             <Link to="createArray">Create Array</Link><br />
             <Link to="linearSortArray">Linear Sort Array</Link><br />
             <Link to="stackAndQueue">Stack and Queue</Link><br />
-            <Link to="popAndDequeue">Pop and Dequeue</Link>
+            <Link to="popAndDequeue">Pop and Dequeue</Link><br />
+            <Link to="addElementToSinglyLinkedList">Add Element to Singly Linked List</Link><br />
+            <Link to="removeElementFromSinglyLinkedList">Remove Element from Singly Linked List</Link><br />
+            <Link to="updateElementFromDoublyLinkedList">Update Element from Doubly Linked List</Link><br />
         </div>
     );
 }
